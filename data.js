22054670878f6f6efc4da95729e960be87a7e69b
Nick <lis@chaikovskie.com>
2021-12-19 23:25:08 +0300

    deploy

---
+++ data.js
@@ -0,0 +1,150 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "Лаборатория Технологий",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -2.350530963276146,
+        "pitch": 0.140496963852069,
+        "fov": 1.3599075722421956
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4106169069982943,
+          "pitch": 0.11527883118596094,
+          "rotation": 5.497787143782138,
+          "target": "1--media"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3334053318033696,
+          "pitch": 0.5553542903346873,
+          "title": "Ноутбук MSI<br>",
+          "text": "i7 10-gen<br><div>1660 TI</div><div>32gb RAM<br>1TB SSD<br>Win 10 Pro<br></div>"
+        },
+        {
+          "yaw": 2.595573944236449,
+          "pitch": 0.32280472248130465,
+          "title": "Стол для сборки прототипов<br>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -1.9801998381322594,
+          "pitch": -0.18755409453483374,
+          "title": "Lorem Ipsum<br>",
+          "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim \nveniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea \ncommodo consequat."
+        }
+      ]
+    },
+    {
+      "id": "1--media",
+      "name": "Лаборатория MEDIA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -1.2476905931871585,
+        "pitch": 0.08725678095667,
+        "fov": 1.3599075722421956
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0753074973241574,
+          "pitch": 0.04188731732008932,
+          "rotation": 4.71238898038469,
+          "target": "2---"
+        },
+        {
+          "yaw": -1.9633461780278854,
+          "pitch": 0.04155588517618014,
+          "rotation": 1.5707963267948966,
+          "target": "0--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2---",
+      "name": "Лаборатория изготовления прототипов",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -1.3969796802579424,
+        "pitch": 0.23574112618319631,
+        "fov": 1.3599075722421956
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9713019750981928,
+          "pitch": 0.12150830685859226,
+          "rotation": 1.5707963267948966,
+          "target": "1--media"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Demo RSSU's Panorama",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
